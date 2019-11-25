3958ceeecf743ee88833b224aeda3cd8791a6369
Dzengiz Tafa <dzengiz.tafa@gmail.com>
2019-11-25 23:03:07 +0100

    Added padding & some basic styling to our barchart to make it look more visually appealing

--- d3_introduction-02_barChart/README.md
+++ d3_introduction-02_barChart/README.md
@@ -1,32 +1,9 @@
-# Webpack Frontend Starterkit
+# Bar chart using D3
 
-[![Dependabot badge](https://flat.badgen.net/dependabot/wbkd/webpack-starter?icon=dependabot)](https://dependabot.com/)
+### Description
+This bar chart is showing the population of the top 10 most populated countries
 
-A lightweight foundation for your next webpack based frontend project.
+### Data source
+[United Nations: World Population Prospects 2017](https://esa.un.org/unpd/wpp/Download/Standard/Population/)
 
 
-### Installation
-
-```
-npm install
-```
-
-### Start Dev Server
-
-```
-npm start
-```
-
-### Build Prod Version
-
-```
-npm run build
-```
-
-### Features:
-
-* ES6 Support via [babel](https://babeljs.io/) (v7)
-* SASS Support via [sass-loader](https://github.com/jtangelder/sass-loader)
-* Linting via [eslint-loader](https://github.com/MoOx/eslint-loader)
-
-When you run `npm run build` we use the [mini-css-extract-plugin](https://github.com/webpack-contrib/mini-css-extract-plugin) to move the css to a separate file. The css file gets included in the head of the `index.html`.
--- d3_introduction-02_barChart/src/scripts/index.js
+++ d3_introduction-02_barChart/src/scripts/index.js
@@ -23,7 +23,7 @@ const generateBarChart = data => {
 
     // Set up the d3 margin convention
     // https://bl.ocks.org/mbostock/3019563
-    const margin = {top: 20, right: 20, bottom:20, left:100};
+    const margin = {top: 20, right: 20, bottom:30, left:150};
     const innerWidth = width - margin.right - margin.left;
     const innerHeight = height - margin.top - margin.bottom;
 
@@ -33,15 +33,15 @@ const generateBarChart = data => {
         // The data domain going from 0 to the max of all the values
         .domain([0, max(data, d => xValue(d))])
 
-        // The visual range along the x scale in pixels
-        // going from 0 to the max width of the svg
+        // The visual range along the x scale in pixels going from 0 to the max width of the svg
         .range([0, innerWidth]);
 
     // Generate an instance of scaleBand for our Y axis
     const yScale = scaleBand()
         // The data domain containing all the countries
         .domain(data.map(d => yValue(d)))
-        .range([0, innerHeight]);
+        .range([0, innerHeight])
+        .padding(0.05);
 
     // Generate the inner rectangle which holds our bars
     // using the d3 margin convention
@@ -50,15 +50,15 @@ const generateBarChart = data => {
 
     // Generate the Y axis group element by "calling" axisLeft(yScale)
     // after the new group gets appended using chaining
-    const yScaleGroup = innerRect.append('g').call(axisLeft(yScale));
+    innerRect.append('g').call(axisLeft(yScale));
 
     // Generate the X axis group element by "calling" axisBottom(xScale)
     // after the new group gets appended using chaining
-    const xScaleGroup = innerRect.append('g').call(axisBottom(xScale))
+    innerRect.append('g').call(axisBottom(xScale))
         .attr('transform', `translate(0, ${innerHeight})`);
 
-        // D3 data mapping to map all the data to the future
-    // rectangles for our bar chart (data join)
+    // D3 data mapping to map all the data to the future rectangles
+    // for our bar chart (data join)
     // https://www.amphinicy.com/blog/post/manipulating-svg-using-d3js-library
     innerRect.selectAll('rect')
         .data(data)
