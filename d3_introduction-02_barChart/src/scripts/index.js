6c1de89e454d9ca47df1bcc13e2d69d01a9f3d78
Dzengiz Tafa <dzengiz.tafa@gmail.com>
2019-11-25 20:41:24 +0100

    CVS file can now be loaded through D3 which contains data for our bar chart

--- d3_introduction-02_barChart/src/scripts/index.js
+++ d3_introduction-02_barChart/src/scripts/index.js
@@ -1,3 +1,26 @@
+// Imports
+//////////
+
+// D3
+import { select, csv } from 'd3';
+
+// Styling
 import '../styles/index.scss';
 
-console.log('webpack starterkit');
+
+// Functionality
+////////////////
+
+// Fetch the svg from the dom
+const svg = select('svg');
+
+// Set the width & height for the bar chart svg
+const width = +svg.attr('width');
+const height = +svg.attr('height');
+
+// Load the data from the population.csv file using d3
+csv('../src/dataFiles/population.csv')
+.then(data => {
+    // Resulting data is converted to an array of objects
+    console.log(data);
+});
