8811503b104ceb95ff60762d8ec1c8d165ed485a
Dzengiz Tafa <dzengiz.tafa@gmail.com>
2019-11-25 22:23:29 +0100

    created value Accessors to make the code more generic

--- d3_introduction-02_barChart/src/scripts/index.js
+++ d3_introduction-02_barChart/src/scripts/index.js
@@ -17,11 +17,15 @@ import '../styles/index.scss';
  ************************************/
 const generateBarChart = data => {
 
+    // Generate the value accessors
+    const xValue = d => d.population;
+    const yValue = d => d.country;
+
     // Generate an instance of scaleLinear for our X axis
     // https://www.dashingd3js.com/d3js-scales
     const xScale = scaleLinear()
         // The data domain going from 0 to the max of all the values
-        .domain([0, max(data, d => d.population)])
+        .domain([0, max(data, d => xValue(d))])
 
         // The visual range along the x scale in pixels
         // going from 0 to the max width of the svg
@@ -30,7 +34,7 @@ const generateBarChart = data => {
     // Generate an instance of scaleBand for our Y axis
     const yScale = scaleBand()
         // The data domain containing all the countries
-        .domain(data.map(d => d.country))
+        .domain(data.map(d => yValue(d)))
         .range([0, height]);
 
     console.log(xScale.domain());
@@ -45,8 +49,8 @@ const generateBarChart = data => {
         .enter()
         .append('rect')
             // Assign y to the country values
-            .attr('y', d => yScale(d.country))
-            .attr('width', d => xScale(d.population)) // Assign the bar width to the xScale using the population field
+            .attr('y', d => yScale(yValue(d)))
+            .attr('width', d => xScale(xValue(d))) // Assign the bar width to the xScale using the population field
             .attr('height', yScale.bandwidth()); // Assign the height to the bandwidth of all the bars
 };
 
