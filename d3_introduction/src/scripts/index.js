2bfdb0e61836d90c1c0281b50da0fae9d49665cd
Dzengiz Tafa <dzengiz.tafa@gmail.com>
2019-11-25 15:22:18 +0100

    added missing files

---
+++ d3_introduction/src/scripts/index.js
@@ -0,0 +1,77 @@
+// Imports
+//////////
+
+// D3
+import { select, arc } from 'd3';
+
+// Styling
+import '../styles/index.scss';
+
+
+// Functionality
+////////////////
+
+// Fetch the svg from the dom
+const svg = select('svg');
+
+// Set the width & height for the smiley face svg
+const width = +svg.attr('width');
+const height = +svg.attr('height');
+
+// Add a group to add elements to with similar properties
+const smileyFace = svg.append('g')
+    .attr('transform', `translate(${width/2}, ${height/2})`);
+
+// Add the circle for the face
+const circle = smileyFace.append('circle')
+    .attr('r', height / 2)
+    .attr('fill', 'yellow')
+    .attr('stroke', 'black');
+
+// Set the constants to use when drawing the face
+const eyeSpacing = 100;
+const eyeYOffset = -70;
+const eyeRadius = 30;
+const eyebrowWidth = 50;
+const eyebrowHeight = 20;
+const eyebrowYOffset = -70;
+
+// Add the eyes group to add elements to with similar properties
+const eyesGroup = smileyFace.append('g')
+    .attr('transform', `translate(0, ${eyeYOffset})`);
+
+// Draw the left Eye
+const leftEye = eyesGroup.append('circle')
+    .attr('r', eyeRadius)
+    .attr('cx', - eyeSpacing);
+
+// Draw the right eye
+const rightEye = eyesGroup.append('circle')
+    .attr('r', eyeRadius)
+    .attr('cx', + eyeSpacing);
+
+// Draw the left eyebrow
+const leftEyebrow = eyesGroup.append('rect')
+    .attr('x', -eyeSpacing - eyebrowWidth / 2)
+    .attr('y', eyebrowYOffset)
+    .attr('width', eyebrowWidth)
+    .attr('height', eyebrowHeight);
+
+// Draw the right eyebrow
+const rightEyebrow = eyesGroup.append('rect')
+    .attr('x', eyeSpacing - eyebrowWidth / 2)
+    .attr('y', eyebrowYOffset)
+    .attr('width', eyebrowWidth)
+    .attr('height', eyebrowHeight);
+
+
+// Draw the right eyebrow
+
+// Draw the mouth
+const mouth = smileyFace.append('path')
+    .attr('d', arc()({
+        innerRadius: 150,
+        outerRadius: 170,
+        startAngle: Math.PI / 2,
+        endAngle: Math.PI * 3 / 2
+    }));
