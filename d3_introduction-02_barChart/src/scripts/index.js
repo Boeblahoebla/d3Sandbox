63949230f7c19a2fbf325369f89c02216714c1c6
Dzengiz Tafa <dzengiz.tafa@gmail.com>
2019-11-25 21:09:15 +0100

    Bars are now generated for each data row... having equal height & width & stacked on top of each other

--- d3_introduction-02_barChart/src/scripts/index.js
+++ d3_introduction-02_barChart/src/scripts/index.js
@@ -8,6 +8,27 @@ import { select, csv } from 'd3';
 import '../styles/index.scss';
 
 
+// Helper functions
+///////////////////
+
+/**
+ * Function to generate the bar chart
+ * @parameter: Array
+ ************************************/
+const generateBarChart = data => {
+    // D3 data mapping to map all the data to the future
+    // rectangles for our bar chart (data join)
+    // https://www.amphinicy.com/blog/post/manipulating-svg-using-d3js-library
+    svg.selectAll('rect')
+        .data(data)
+        .enter()
+        .append('rect')
+        .attr('width', 300)
+        .attr('height', 300)
+};
+
+
+
 // Functionality
 ////////////////
 
@@ -20,7 +41,18 @@ const height = +svg.attr('height');
 
 // Load the data from the population.csv file using d3
 csv('../src/dataFiles/population.csv')
-.then(data => {
-    // Resulting data is converted to an array of objects
-    console.log(data);
+    .then(data => {
+        // Resulting data is converted to an array of objects
+
+        // Loop through each row 'D'atarow & read the population field
+        // as a number using the +prefix. Then Multiply by 1000
+        // Since data is in "thousands" (x thousand ppl)
+        data.forEach(d => {
+            d.population = +d.population * 1000;
+        });
+
+        // Generate the bar chart
+        generateBarChart(data);
+
+        console.log(data);
 });
